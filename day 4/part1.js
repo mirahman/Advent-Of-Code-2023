3903639f013fc2da48b1671ed4baf17d94765f7a
Mizanur Rahman <mizanur.rahman@gmail.com>
2023-12-04 00:42:06 -0500

    day 4

---
+++ day 4/part1.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+try {
+    const txt = fs.readFileSync('./input1.txt', 'utf8');
+    const cards = txt.split('\n')
+    let total = 0;
+    for (let card of cards) {
+        let map = new Map();
+        let matchCount = 0;
+
+        let tmp = card.split(":")[1].split("|");
+        let winningNumbers = tmp[0].trim().split(/\s+/);
+        
+        for(let num of winningNumbers) {
+            map.set(num);
+        }
+        let myNumbers =tmp[1].trim().split(/\s+/);
+        console.log(winningNumbers, myNumbers);
+        for(let num of myNumbers) {
+            if(map.has(num)) {
+                matchCount++;
+            }
+        }
+        if(matchCount) {
+            total += Math.pow(2, matchCount-1);
+        }
+    }
+    console.log(total)
+} catch (err) {
+    console.error("error", err);
+}
---
+++ day 4/part2.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+try {
+    const txt = fs.readFileSync('./input1.txt', 'utf8');
+    const cards = txt.split('\n')
+    let counter = new Array(cards.length).fill(1);
+    let total = 0;
+    let row = 0;
+    for (let card of cards) {
+        let map = new Map();
+        let matchCount = 0;
+        let tmp = card.split(":")[1].split("|");
+        let winningNumbers = tmp[0].trim().split(/\s+/);
+        let myNumbers = tmp[1].trim().split(/\s+/);
+        
+        for (let num of winningNumbers) {
+            map.set(num);
+        }
+        for (let num of myNumbers) {
+            if (map.has(num)) {
+                matchCount++;
+            }
+        }
+        for (let y = 0; y < counter[row]; y++) {
+            for (let x = 0; x < matchCount; x++) {
+                counter[x + row + 1] += 1;
+            }
+        }
+        row++;
+    }
+    total = counter.reduce((a, c) => a + c, 0,);
+    console.log(total)
+} catch (err) {
+    console.error("error", err);
+}
