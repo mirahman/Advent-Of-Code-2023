94a038cbd13799a7d17dc424753fd41f89496bd4
Mizanur Rahman <mizanur.rahman@gmail.com>
2023-12-12 13:33:02 -0500

    day 12

---
+++ day 12/part2.js
@@ -0,0 +1,52 @@
+const { Console } = require('console');
+const fs = require('fs');
+try {
+    const txt = fs.readFileSync('./input1.txt', 'utf8');
+    const lines = txt.split('\n');
+    let total = 0;
+    const DAMAGED = "#";
+    const WORKING = ".";
+    const UNKNOWN = "?";
+    let map = new Map();
+
+    const solvePlacement = (springs, groups, current = 0, local = 0, processed = 0) => {
+        let mapkey = current + "_" + local + "_" + processed;
+        if (map.has(mapkey)) return map.get(mapkey);
+
+        //console.log(current, local, processed);
+        if (current == springs.length) {
+            if (local == groups.length - 1 && processed == groups[local]) { local++; processed = 0; }
+            if (groups.length == local && processed == 0)
+                return 1;
+            return 0;
+        }
+        
+        let result = 0;
+
+        if (springs[current] == WORKING || springs[current] == UNKNOWN) // ...operational
+        {
+            if (processed == 0)
+                result += solvePlacement(springs, groups, current + 1, local, 0);
+            else if (local < groups.length && processed == groups[local])
+                result += solvePlacement(springs, groups, current + 1, local + 1, 0);
+        }
+
+        if (springs[current] == DAMAGED || springs[current] == UNKNOWN) {
+            result += solvePlacement(springs, groups, current + 1, local, processed + 1);
+        }
+
+        map.set(mapkey, result);
+        return result;
+    }
+
+    for (let line of lines) {
+        let tmp = line.split(" ");
+        let arr = tmp[1].split(",").map(Number);
+        let str = tmp[0].trim();
+        total += solvePlacement(str + "?" + str + "?" + str + "?" + str + "?" + str, [...arr, ...arr, ...arr, ...arr, ...arr]);
+        map = new Map();
+    }
+    console.log(total);
+} catch (err) {
+    console.error("error", err);
+}
